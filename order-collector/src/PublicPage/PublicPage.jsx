0072cf4ef2baa8fcb9e6c4ca27fdf86763f886b4
Anakin <kaungkaung272005@gmail.com>
2025-08-14 14:38:11 +0630

    entry page demo

--- order-collector/src/PublicPage/PublicPage.jsx
+++ order-collector/src/PublicPage/PublicPage.jsx
@@ -1,36 +1,36 @@
 import { Link } from "react-router-dom";
-
-function PublicPage(){
+import "./PublicPage.css";
+function PublicPage() {
     return (
-    <div>
-        <h1>FlashCollect</h1>
-        <p>Welcome to FlashCollect!</p>
+        <div className="publicConext">
+            <div className="leftSide">
+                <div className="textBox">
+                    <h1>FlashCollect</h1>
+                    <p style={{ fontSize: "34px", color: "#c71585", fontWeight: "bold" }}>Welcome to FlashCollect!</p>
+                    <p>Your all in one order collection management system,
+                        mainly aimed for entrepreneurs and small business owners.
+                        <br></br>
+                        Struggling to keep daily orders organized?
+                        No worries, we’re here to simplify your business operations and help you save time.</p>
 
-        <p>Your all in one order collection management system, 
-mainly aimed for entrepreneurs and small business owners.
-<br></br>
-Struggling to keep daily orders organized? 
-No worries, we’re here to simplify your business operations and help you save time.</p>
-
-<p>New here?, please register to use our system.<br></br>
-For registered users, please login to your existing account.</p>
-
-<div><p>Getting Started
-
-    <br></br>
-
-    Register or login to start using our system....
-
-    <Link to={"/register"}>Register</Link>   
-    <Link to={"/login"}>Login</Link>
-</p>
-
-<p></p>
-
-</div>
-    </div>);
+                    <p>
+                        <b>New here?</b>, please register to use our system.<br></br>
+                        <b>For registered users</b>, please login to your existing account.
+                    </p>
+                </div>
+            </div>
+            <div className="rightSide">
+                <h1>Getting Started</h1>
+                <br></br>
+                <p>
+                    Register or login to start using our system....
+                </p>
+                <Link to={"/register"}>Register</Link>
+                <Link to={"/login"}>Login</Link>
+            </div>
+        </div >);
 
 
-    
+
 }
 export default PublicPage;
