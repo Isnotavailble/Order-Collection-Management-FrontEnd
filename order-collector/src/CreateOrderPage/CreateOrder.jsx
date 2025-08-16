a7a393d0f152373a488bcfc2dd1428e7d7d48515
Anakin <kaungkaung272005@gmail.com>
2025-08-16 11:33:20 +0630

    sending request can be done

--- order-collector/src/CreateOrderPage/CreateOrder.jsx
+++ order-collector/src/CreateOrderPage/CreateOrder.jsx
@@ -10,39 +10,148 @@ const MenuBtn = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" f
 </svg>
 
 function CreateOrder() {
+    // product rows' data 
+    //Date.now() just generate non-repeat number so I used it as id
     let [row, setRow] = useState([{
-        id: Date.now(), value: ""
-    }]);//just 0 and 1 for rendering rows
-    let orderStatus = ["pending", "cancelled", "completed"];
-    let buttonsGroup = useRef({});
+        "id": Date.now(),
+        "productName": "",
+        "quantity": "",
+        "price": 0
+    }]);
+    //seletected type 
+    let [orderType, setOrderType] = useState("Delivery");
+
+    //customer data and order date data
+    let [rightSideData, setRightSideData] = useState({
+        "customer": {
+            "name": "",
+            "address": "",
+            "phone_number": ""
+        },
+        "order": {
+            "order_status": "pending",
+            "start_date": "",
+            "end_date": ""
+        }
+    });
+    //orderType for btn
+    //update : the className should be orderType so keep that in mind !     
+    let orderStatus = ["Delivery", "PickUp"];
+    let buttonsGroup = useRef({});//DOM obj for buttons 
+    //make default if enter this page again
     useEffect(() => {
-        if (buttonsGroup.current["status-btn-gp"]){
+        if (buttonsGroup.current["status-btn-gp"]) {
             buttonsGroup.current["status-btn-gp"].style.maxHeight = "0px";
         }
-    },[]);
+    }, []);
+    //product table rows generator
     function generateRow(r) {
         if (r) {
             return (
                 r.map((el) =>
                     <div className="row" key={el.id}>
-                        <input type="text" placeholder="Type here" />
-                        <input type="text" placeholder="Type here" />
-                        <input type="number" placeholder="1" min="1" />
+                        <input type="text" placeholder="Type here"
+                            onChange={e => inputHandle(e, el.id, "productName")} />
+                        <input type="text" placeholder="Type here"
+                            onChange={e => inputHandle(e, el.id, "price")}
+                        />
+                        <input type="number" placeholder="1" min="1"
+                            onChange={e => inputHandle(e, el.id, "quantity")}
+                        />
                         <button onClick={() => deleteRow(el.id)}><img id="trash-pic" src={TrashPic} /></button>
                     </div>));
         }
         return null;
     }
+    // input handle for product table
+    // in json obj,to change the key dynamically : use [key] : value what the fuck is even this fuck js
+    function inputHandle(e, mapID, fieldName) {
+        //console.log(fieldName);
+        setRow(prev =>
+            prev.map(item => item.id === mapID ? { ...item, [fieldName]: e.target.value } : item))
+        //console.log("Data input : ", e.target.value);
+    }
+    //input handle for customer and order date 
+    function inputHandle_V1(e, objName, fieldName) {
+        //console.log("obj : " + objName + "\nfieldName : " + fieldName);
+        setRightSideData(prev => ({
+            ...prev,
+            [objName]: {
+                ...prev[objName],
+                [fieldName]: e.target.value
+            }
+        }));
+    }
+    //to see rightside data update 
+    useEffect(() => {
+        console.log("right Data : ", rightSideData);
+    }, [rightSideData]);
+    //to see product row data
+    useEffect(() => {
+        console.log("Data in state : ", row);
+    }, [row]);
+    //to see selected order type
+    useEffect(() => {
+        console.log("Order type : ", orderType);
+    }, [orderType]);
     function deleteRow(idToRemoveRow) {
         setRow(p => p.filter((el, id) => el.id !== idToRemoveRow));
         console.log("Deleted : ", idToRemoveRow);
     }
     function addRow() {
-        setRow(p => [...p, { id: Date.now(), value: "" }]);
+        setRow(p => [...p, { "id": Date.now(), "productName": "", "price": 0 }]);
         console.log("Data added");
     }
-    function showAndHideHandler(element){
-        element.style.maxHeight = element.style.maxHeight !== "0px"? "0px" : "150px";
+    //for orderstatus drop down 
+    function showAndHideHandler(element) {
+        element.style.maxHeight = element.style.maxHeight !== "0px" ? "0px" : "150px";
+    }
+    //because of spring security,have to enter the generated password from spring api 
+    //send request the data to api
+    function postData(url) {
+        let request = {
+            "orderStatus": rightSideData["order"].order_status,
+            "orderDate": rightSideData["order"].start_date,
+            "dueDate": rightSideData["order"].end_date,
+            "orderType": orderType,
+            "userID": 2,//Test
+            "customer": {
+                "customerName": rightSideData["customer"].name,
+                "phoneNumber": rightSideData["customer"].phone_number,
+                "customerAddress": rightSideData["customer"].address
+            },
+            "orderItems": row.map((el, index) => (
+                {   "quantity" : el.price,
+                    "product": {
+                        "productName": el.productName,
+                        "productCategory": "Not Yet",
+                        "price": el.price
+                    }
+                }
+            ))
+        }
+        console.log("request : " ,request);
+        
+        fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(request),
+            credentials : "include"
+        })
+        .then(response => {
+            if (!response.ok){
+                throw new Error ("Error while sending order");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Response fro api : ",data)
+        })
+        .catch(error => {
+            console.log("Error : " + error.message);
+        });
     }
     return (
         <div className="create-order-content">
@@ -52,15 +161,15 @@ function CreateOrder() {
                     <div className="form-overlay">
                         <div className="form-left-side">
                             <b>Customer Name</b>
-                            <input type="text" placeholder="Type here" />
+                            <input type="text" placeholder="Type here" onChange={e => inputHandle_V1(e, "customer", "name")} />
                             <b>Customer's Address</b>
-                            <input type="text" placeholder="Type here" />
+                            <input type="text" placeholder="Type here" onChange={e => inputHandle_V1(e, "customer", "address")} />
                             <b>Customer's PhoneNumber</b>
-                            <input type="text" placeholder="Type here" />
+                            <input type="text" placeholder="Type here" onChange={e => inputHandle_V1(e, "customer", "phone_number")} />
                             <b>Order Date</b>
-                            <input type="date" placeholder="Type here" />
+                            <input type="date" placeholder="Type here" onChange={e => inputHandle_V1(e, "order", "start_date")} />
                             <b>Delivery/PickUp Date </b>
-                            <input type="date" placeholder="Type here" />
+                            <input type="date" placeholder="Type here" onChange={e => inputHandle_V1(e, "order", "end_date")} />
                         </div>
                         <div className="form-right-side">
                             <div className="product-table">
@@ -75,11 +184,12 @@ function CreateOrder() {
                         </div>
                     </div>
                     <div className="btn-row">
-                        <button id="sendBtn"> {SendOrderPic} <b> Create Order </b></button>
+                        <button id="sendBtn" onClick={e => postData("http://localhost:8080/api/auth/uploadOrder")}> {SendOrderPic} <b> Create Order </b></button>
                         <div className="status-btn-container">
-                            <button id="sendBtn" onClick={() => showAndHideHandler(buttonsGroup.current["status-btn-gp"])}> {MenuBtn} <b>Order Status</b> </button>
-                            <div className="status-btn-gp" ref={ el => {if (el) buttonsGroup.current["status-btn-gp"] = el}}>
-                                {orderStatus.map((el,index) => <button key={index}>{el}</button>)}
+                            <button id="sendBtn" onClick={() => showAndHideHandler(buttonsGroup.current["status-btn-gp"])}> {MenuBtn} <b>Order Type {orderType ? " : " + orderType : null}</b> </button>
+                            <div className="status-btn-gp" ref={el => { if (el) buttonsGroup.current["status-btn-gp"] = el }}>
+                                {orderStatus.map((el, index) =>
+                                    <button key={index} onClick={() => setOrderType(el)}>{el} </button>)}
                             </div>
                         </div>
                     </div>
