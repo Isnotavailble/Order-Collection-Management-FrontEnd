b25fb9ea4cc5b472b7e2ab37dbb4f9d8498aa4b5
Anakin <kaungkaung272005@gmail.com>
2025-08-21 19:23:20 +0630

    update edit model

--- order-collector/src/EditOrdersPage/EditOrder.jsx
+++ order-collector/src/EditOrdersPage/EditOrder.jsx
@@ -16,12 +16,13 @@ const MenuBtn = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" f
 function EditOrders(props) {
     //data from api
     let [order_data, setOrderData] = useState(null);
+    //for null check
 
     //search bar
     let [requestOrderId, setRequestOrderId] = useState(0);
     //for prodcut rows
     let [row, setRow] = useState([
-        { "id": Date.now(), "productName": "", "price": "", "quantity": "", "delete": false }]);
+        { "id": Date.now(), "order_item_id": null, "productName": "", "price": 0, "quantity": 0, "delete": false }]);
     // deleted product rows
     let [deletedItem, setDeletedItem] = useState([]);
     //customer data and order date data
@@ -51,38 +52,47 @@ function EditOrders(props) {
     function generateRow(r) {
         if (r) {
             return (
-                r.map((el) =>
-                    <div className="row" key={el.id}>
-                        <input type="text" placeholder="Type here" value={el.productName}
-                            onChange={e => inputHandle(e, el.id, "productName")} />
-                        <input type="text" placeholder="Type here" value={el.price}
-                            onChange={e => inputHandle(e, el.id, "price")}
-                        />
-                        <input type="number" placeholder="1" min="1" value={el.quantity}
-                            onChange={e => inputHandle(e, el.id, "quantity")}
-                        />
-                        <button
-                            onClick={() => {
-                                if (el.productName !== "" && el.price > 0 && el.quantity > 0) {
-                                    el.delete = true;
-                                    setDeletedItem(prev => [...prev, el]);
-                                }
-                                deleteRow(el.id);
-                            }}>
-                            <img id="trash-pic" src={TrashPic} />
-                        </button>
-                    </div>));
+                r.map((el) => {
+                    if (!el.delete)
+                        return (
+                            <div className="row" key={el.id}>
+                                <input type="text" placeholder="Type here" value={el.productName}
+                                    onChange={e => inputHandle(e, el.id, "productName")} />
+                                <input type="number" placeholder="Type here" value={el.price}
+                                    onChange={e => inputHandle(e, el.id, "price")}
+                                />
+                                <input type="number" placeholder="1" min="1" value={el.quantity}
+                                    onChange={e => inputHandle(e, el.id, "quantity")}
+                                />
+                                <button
+                                    onClick={() => {
+                                        if (el.productName !== "" && el.price > 0 && el.quantity > 0) {
+                                            el.delete = true;
+                                            setDeletedItem(prev => [...prev, el]);
+                                        }
+                                        deleteRow(el.id);
+                                    }}>
+                                    <img id="trash-pic" src={TrashPic} />
+                                </button>
+                            </div>);
+                }));
         }
         return null;
     }
 
     //product table add and remove items
     function deleteRow(idToRemoveRow) {
-        setRow(p => p.filter((el, id) => el.id !== idToRemoveRow));
+        //setRow(p => p.filter((el, id) => el.id !== idToRemoveRow));
+        setRow(p => p.map((el, index) => {
+            if (el.id === idToRemoveRow) {
+                el.delete = true;
+            }
+            return el;
+        }));
         console.log("Deleted : ", idToRemoveRow);
     }
     function addRow() {
-        setRow(p => [...p, { "id": Date.now(), "productName": "", "price": "", "quantity": 0, "delete": false }]);
+        setRow(p => [...p, { "id": Date.now(), "order_item_id": null, "productName": "", "price": 0, "quantity": 0, "delete": false }]);
         console.log("Data added");
     }
     //animations
@@ -90,13 +100,16 @@ function EditOrders(props) {
         element.style.maxHeight = element.style.maxHeight !== "0px" ? "0px" : "150px";
     }
     useEffect(() => {
-        refObj.current["status-btn-gp"].style.maxHeight = "0px";
+        if (refObj.current["status-btn-gp"]) refObj.current["status-btn-gp"].style.maxHeight = "0px";
     }, []);
 
     //input handlers
     // input handle for product table
     function inputHandle(e, mapID, fieldName) {
-        console.log(fieldName);
+        //console.log("input value : ", filedValue);
+        if (fieldName === "price" || fieldName === "quantity") {
+            e.target.value = e.target.value !== "" ? parseFloat(e.target.value) : 0;
+        }
         setRow(prev =>
             prev.map(item => item.id === mapID ? { ...item, [fieldName]: e.target.value } : item))
         //console.log("Data input : ", e.target.value);
@@ -134,6 +147,7 @@ function EditOrders(props) {
             setRow(prev => order_data.orderItem.map((item, index) => (
                 {
                     "id": Date.now() + index,
+                    "order_item_id": item.order_item_id,
                     "productName": item.product_name,
                     "price": item.product_price,
                     "quantity": item.quantity,
@@ -177,8 +191,9 @@ function EditOrders(props) {
                 console.log("order : ", data);
             })
             .catch(error => {
-                if (error.message.includes("Fetch" || "fetch")) {
+                if (error.message.toLocaleLowerCase().includes("fetch")) {
                     setResponse("Unable to find order");
+                    return;
                 }
                 setResponse(error.message);
                 console.log("Error", error.message);
@@ -186,77 +201,151 @@ function EditOrders(props) {
     }
 
     //update order
-    //function updateOrder() {
-    //fetch()
-    //}
+    //update api has two features
+    //1. if order item id is null then it will assume as new item
+    //2. if order iten id is NOT NULL then it will assume as existing item 
+    function updateOrder() {
 
-    return (<>
-        {response.toLocaleLowerCase().includes("not found") ? <OverlayV1 message={response} setResponse={setResponse} /> : null}
-        <CreateOrderStyleWrapper>
-            <div className="create-order-content">
-                <h1>Edit Order</h1>
-                <input className="input-bar" type="number" placeholder="enter order ID" min="1"
-                    ref={el => { if (el) refObj.current["input-bar"] = el }}
-                    onChange={e => {
-                        if (e.target.value > 0)
-                            setRequestOrderId(e.target.value);
-                        else {
-                            e.target.value = null;
-                            setRequestOrderId(null);
-                        }
-                    }}
-                    onKeyDown={e => {
-                        if (e.key === "Enter") {
-                            e.target.value = null;
-                            getOrderById(requestOrderId);
-                        }
-                    }}
-                /> <button className="search-btn" onClick={() => { refObj.current["input-bar"].value = null; getOrderById(requestOrderId); }}>Search</button>
-                <div className="create-card">
-                    <div className="form">
-                        <h1>Order ID {rightSideData["order"].order_id}</h1>
-                        <div className="form-overlay">
-                            <div className="form-left-side">
-                                <b>Customer Name</b>
-                                <input type="text" placeholder="Type here" value={rightSideData["customer"].name} onChange={e => inputHandle_V1(e, "customer", "name")} />
-                                <b>Customer's Address</b>
-                                <input type="text" placeholder="Type here" value={rightSideData["customer"].address} onChange={e => inputHandle_V1(e, "customer", "address")} />
-                                <b>Customer's PhoneNumber</b>
-                                <input type="text" placeholder="Type here" value={rightSideData["customer"].phone_number} onChange={e => inputHandle_V1(e, "customer", "phone_number")} />
-                                <b>Order Date</b>
-                                <input type="date" placeholder="Type here" value={rightSideData["order"].start_date} onChange={e => inputHandle_V1(e, "order", "start_date")} />
-                                <b>Delivery/PickUp Date </b>
-                                <input type="date" placeholder="Type here" value={rightSideData["order"].end_date} onChange={e => inputHandle_V1(e, "order", "end_date")} />
-                            </div>
-                            <div className="form-right-side">
-                                <div className="product-table">
-                                    <div className="row rowHeader">
-                                        <b>Product Name</b>
-                                        <b>Price</b>
-                                        <b>Quantity</b>
+        //wrapping data into DTO
+        //in this DTO only orderItemId has right to have null value
+        //other fidlds will be filtered so they must have value
+        let requestDTO = {
+            "orderID": rightSideData["order"].order_id,
+            "dueDate": rightSideData["order"].end_date.trim(),
+            "orderDate": rightSideData["order"].start_date.trim(),
+            "orderType": selectedOrderType,
+            "customerName": rightSideData["customer"].name.trim(),
+            "customerAddress": rightSideData["customer"].address.trim(),
+            "phoneNumber": rightSideData["customer"].phone_number.trim(),
+            "orderItems": row.filter((item, _) => item.productName !== "" && item.price > 0 && item.quantity > 0)
+                .map((item, _) => {
+                    return ({
+                        "orderItemID": item.order_item_id,
+                        "productName": item.productName.trim(),
+                        "price": item.price,
+                        "quantity": item.quantity,
+                        "delete": item.delete
+                    });
+
+                })
+        }
+        console.log("requestDTO : ", requestDTO);
+        //break point if null 
+        let HasNull = Object.values(requestDTO).filter((item, _) => item === null || item === "").length > 0 ? true : false;
+        if (HasNull) {
+            console.log("has null ", HasNull);
+            setResponse("PLease check your order's field");
+            return;
+        }
+
+
+        fetch("http://localhost:8080/api/auth/orders/edit", {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(requestDTO)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(error => {
+                        throw new Error(error.message || "Unknown Error");
+                    });
+                }
+                return response.json();
+            }).then(
+                data => {
+                    setResponse(data.message);
+                    console.log("upload successfully data : ", data);
+                }
+            ).catch(
+                error => {
+                    console.log("Error : ", error.message);
+
+                    if (error.message.includes("fetch")) {
+                        setResponse("Unable to update");
+                        return;
+                    }
+                    setResponse(error.message);
+                }
+            );
+
+    }
+
+
+    return (
+        <div>
+
+            {response.toLocaleLowerCase().includes("unable") ||
+                response.toLocaleLowerCase().includes("not found") ||
+                response === "PLease check your order's field" ||
+                response.toLocaleLowerCase().includes("successfully")
+                ? <OverlayV1 message={response} setResponse={setResponse} /> : null}
+            <CreateOrderStyleWrapper>
+                <div className="create-order-content">
+                    <h1>Edit Order</h1>
+                    <input className="input-bar" type="number" placeholder="enter order ID" min="1"
+                        ref={el => { if (el) refObj.current["input-bar"] = el }}
+                        onChange={e => {
+                            if (e.target.value > 0)
+                                setRequestOrderId(e.target.value);
+                            else {
+                                e.target.value = null;
+                                setRequestOrderId(null);
+                            }
+                        }}
+                        onKeyDown={e => {
+                            if (e.key === "Enter") {
+                                e.target.value = null;
+                                getOrderById(requestOrderId);
+                            }
+                        }}
+                    /> <button className="search-btn" onClick={() => { refObj.current["input-bar"].value = null; getOrderById(requestOrderId); }}>Search</button>
+                    <div className="create-card">
+                        <div className="form">
+                            <h1>Order ID {rightSideData["order"].order_id}</h1>
+                            <div className="form-overlay">
+                                <div className="form-left-side">
+                                    <b>Customer Name</b>
+                                    <input type="text" placeholder="Type here" value={rightSideData["customer"].name} onChange={e => inputHandle_V1(e, "customer", "name")} />
+                                    <b>Customer's Address</b>
+                                    <input type="text" placeholder="Type here" value={rightSideData["customer"].address} onChange={e => inputHandle_V1(e, "customer", "address")} />
+                                    <b>Customer's PhoneNumber</b>
+                                    <input type="text" placeholder="Type here" value={rightSideData["customer"].phone_number} onChange={e => inputHandle_V1(e, "customer", "phone_number")} />
+                                    <b>Order Date</b>
+                                    <input type="date" placeholder="Type here" value={rightSideData["order"].start_date} onChange={e => inputHandle_V1(e, "order", "start_date")} />
+                                    <b>Delivery/PickUp Date </b>
+                                    <input type="date" placeholder="Type here" value={rightSideData["order"].end_date} onChange={e => inputHandle_V1(e, "order", "end_date")} />
+                                </div>
+                                <div className="form-right-side">
+                                    <div className="product-table">
+                                        <div className="row rowHeader">
+                                            <b>Product Name</b>
+                                            <b>Price</b>
+                                            <b>Quantity</b>
+                                        </div>
+                                        {generateRow(row)}
+                                        <button id="add-product-btn" onClick={() => { addRow(); }}><img id="add-icon" src={PlusPic} /> Add a product</button>
                                     </div>
-                                    {generateRow(row)}
-                                    <button id="add-product-btn" onClick={() => { addRow(); }}><img id="add-icon" src={PlusPic} /> Add a product</button>
                                 </div>
                             </div>
-                        </div>
-                        <div className="btn-row">
-                            <button id="sendBtn" onClick={() => { }}> {SendOrderPic} <b> Confirm Update </b></button>
-                            <div className="status-btn-container">
-                                <button id="sendBtn" onClick={() => showAndHideHandler(refObj.current["status-btn-gp"])}>
-                                    {MenuBtn} <b> OrderType : {selectedOrderType} </b>
-                                </button>
-                                <div className="status-btn-gp" ref={(el) => { if (el) refObj.current["status-btn-gp"] = el; }}>
-                                    {orderType.map((el, index) =>
-                                        <button key={index} onClick={() => { setSelectedOrderType(el); }}>{el} </button>)}
+                            <div className="btn-row">
+                                <button id="sendBtn" onClick={() => { updateOrder(); }}> {SendOrderPic} <b> Confirm Update </b></button>
+                                <div className="status-btn-container">
+                                    <button id="sendBtn" onClick={() => showAndHideHandler(refObj.current["status-btn-gp"])}>
+                                        {MenuBtn} <b> OrderType : {selectedOrderType} </b>
+                                    </button>
+                                    <div className="status-btn-gp" ref={(el) => { if (el) refObj.current["status-btn-gp"] = el; }}>
+                                        {orderType.map((el, index) =>
+                                            <button key={index} onClick={() => { setSelectedOrderType(el); }}>{el} </button>)}
+                                    </div>
                                 </div>
                             </div>
-                            <b id="status-ms" >Error status</b>
                         </div>
                     </div>
                 </div>
-            </div>
-        </CreateOrderStyleWrapper>
-    </>);
+            </CreateOrderStyleWrapper>
+        </div>);
 }
+
 export default EditOrders;
