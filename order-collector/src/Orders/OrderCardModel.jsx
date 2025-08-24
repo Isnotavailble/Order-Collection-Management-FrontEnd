3de14a635ed03b2186898867552196d782635149
Anakin <kaungkaung272005@gmail.com>
2025-08-24 16:12:47 +0630

    card model for orders page

---
+++ order-collector/src/Orders/OrderCardModel.jsx
@@ -0,0 +1,131 @@
+import "./OrderCardModel.css";
+import { useEffect, useRef, useState } from "react";
+const delete_icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+</svg>
+const update_icon = <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+</svg>
+function OrderCardModel({ orderType, data }) {
+    let [status, setStatus] = useState("pending");//selected status
+    let refObj = useRef({});//DOM references
+    let status_options = ["pending", "complete", "cancelled"];
+    let [dataCard, setDataCard] = useState({});
+    useEffect(() => {
+        if (data) {
+            setDataCard(data);
+            return () => { };
+        }
+        setDataCard({
+            "orderType": "Order Type",
+            "orderId": "101",
+            "date": "MM/HH/DD",
+            "createdAt": "MM/HH/DD",
+            "customerName": "Customer Name",
+            "customerAddress": "Customer Address",
+            "phoneNumber": "+91-XXXXXXXXXX",
+            "products": [
+                { "productName": "product name", "price": 0, "quantity": 0 },
+                { "productName": "prodcut name", "price": 0, "quantity": 0 },
+                { "productName": "product name", "price": 0, "quantity": 0 },
+            ],
+            "totalPrice": 0
+        });
+        return () => { };
+    }, [data]);
+
+    //animation drop-down
+    let handle_status_options = () => {
+        if (refObj.current["status-options"]) {
+            refObj.current["status-options"].style.maxHeight = refObj.current["status-options"].style.maxHeight !== "0px" ? "0px" : "100px";
+            refObj.current["status-options"].style.paddingTop = refObj.current["status-options"].style.paddingTop !== "0px" ? "0px" : "10px";
+            refObj.current["status-options"].style.paddingBottom = refObj.current["status-options"].style.paddingBottom !== "0px" ? "0px" : "10px";
+        }
+    }
+    //reset drop-down on mount (page load)
+    useEffect(() => {
+        if (refObj.current["status-options"]) {
+            refObj.current["status-options"].style.maxHeight = "0px";
+            refObj.current["status-options"].style.paddingTop = "0px";
+            refObj.current["status-options"].style.paddingBottom = "0px";
+        }
+    }, []);
+
+    return (
+        <div className="order-card-model-container">
+            <h1>Order Type</h1>
+            <div className="order-card-model-content">
+                <div className="card-row">
+                    <div>
+                        <h3>Order Id</h3>
+                        <p>{dataCard["orderId"]}</p>
+                    </div>
+                    <div>
+                        <h3>Date</h3>
+                        <p>{dataCard["date"]}</p>
+                    </div>
+                    <div>
+                        <h3>Created At</h3>
+                        <p>{dataCard["createdAt"]}</p>
+                    </div>
+                </div>
+                <div className="card-row middle-row">
+                    <div>
+                        <div className="dd">
+                            <h3>Customer</h3>
+                            <p>{dataCard["customerName"]}</p>
+                        </div>
+                        <div className="dd">
+                            <h3>Address</h3>
+                            <p>{dataCard["customerAddress"]}</p>
+                        </div>
+                        <div className="dd">
+                            <h3>Phone Number</h3>
+                            <p>{dataCard["phoneNumber"]}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="products-table">
+                            <div className="table-header">
+                                <h3>Product Name</h3>
+                                <h3>Price</h3>
+                                <h3>Quantity</h3>
+                            </div>
+                            <div className="table-body">
+                                {dataCard["products"] && dataCard["products"].map((el, index) =>
+                                    <div className="table-row" key={index}>
+                                        <p>{el.productName}</p>
+                                        <p>{el.price}</p>
+                                        <p>{el.quantity}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                        <div className="total-price">
+                            <h3>Total Price</h3>
+                            <p>{dataCard["totalPrice"]}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="status-row">
+                <div className="status-display">
+                    <h3>Status :</h3>
+                    <p className="pending-btn" id={`${status}-btn1`}>{status}</p>
+                </div>
+                <button className="update-order-btn"> {delete_icon} Delete Order</button>
+                <div className="update-status-gp">
+                    <button className="update-order-btn" onClick={() => handle_status_options()}>{update_icon} Update Status</button>
+                    <div className="status-options" ref={(el) => { if (el) refObj.current["status-options"] = el }}>
+                        {
+                            status_options.map((el, index) => <button key={index} onClick={() => setStatus(el)} >{el}</button>)
+                        }
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default OrderCardModel;
--- order-collector/src/Orders/OrderDashBoard.jsx
+++ order-collector/src/Orders/OrderDashBoard.jsx
@@ -1,19 +1,23 @@
 
 import "./OrderDashBoard.css"
 import SearchBar from "./SearchBar.jsx";
+import OrderCardModel from "./OrderCardModel.jsx";
 function OrderDashBoard() {
     return (
         <div className="order-dashboard-contianer">
             <h1>Order DashBoard</h1>
             <div className="board-upper-row">
                 <SearchBar />
-
-                <button id="default-btn" >Default</button>
-                <button id="pending-btn">pending</button>
-                <button id="complete-btn">complete</button>
-                <button id="cancelled-btn">cancelled</button>
-
+                <button id="default-btn" className="upper-btn">Default</button>
+                <button id="pending-btn" className="upper-btn">pending</button>
+                <button id="complete-btn" className="upper-btn">complete</button>
+                <button id="cancelled-btn" className="upper-btn">cancelled</button>
             </div>
+            <div className="order-cards-list">
+                <OrderCardModel />
+            </div>
+
+
         </div>
     );
 }
